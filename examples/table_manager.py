7018b121aae7c4099fb34b345ee44d194644fdca
Allisson Azevedo <allisson@gmail.com>
2018-01-10 21:29:55 -0300

    Update readme with TableManager and AbstractHook

---
+++ examples/table_manager.py
@@ -0,0 +1,63 @@
+import asyncio
+from datetime import date
+
+from asyncpg_utils.databases import Database
+from asyncpg_utils.managers import TableManager
+
+loop = asyncio.get_event_loop()
+database = Database('postgresql://postgres:postgres@localhost/asyncpg-utils')
+table_manager = TableManager(database, 'users', pk_field='id', hooks=None)
+user_data = {
+    'name': 'Allisson',
+    'dob': date(1983, 2, 9)
+}
+
+
+async def create_table():
+    conn = await database.get_connection()
+    await conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users(
+            id serial PRIMARY KEY,
+            name text,
+            dob date
+        )
+        """
+    )
+    await conn.close()
+    return True
+
+
+async def table_manager_create():
+    print('table_manager.create, row={!r}'.format(await table_manager.create(user_data)))
+
+
+async def table_manager_list():
+    print('table_manager.list, rows={!r}'.format(await table_manager.list()))
+    print('table_manager.list, only_name_field, rows={!r}'.format(await table_manager.list(fields=['name'])))
+    print('table_manager.list, filter_by_id, rows={!r}'.format(await table_manager.list(filters={'id': 999999})))
+
+
+async def table_manager_detail():
+    print('table_manager.detail, row={!r}'.format(await table_manager.detail(1)))
+    print('table_manager.detail, only_name_field, row={!r}'.format(await table_manager.detail(1, fields=['name'])))
+
+
+async def table_manager_update():
+    user_data['name'] = 'John Doe'
+    print('table_manager.update, row={!r}'.format(await table_manager.update(1, user_data)))
+
+
+async def table_manager_delete():
+    print('table_manager.delete, result={!r}'.format(await table_manager.delete(1)))
+
+
+async def main():
+    print('create_table users, {!r}'.format(await create_table()))
+    await table_manager_create()
+    await table_manager_list()
+    await table_manager_detail()
+    await table_manager_update()
+    await table_manager_delete()
+
+loop.run_until_complete(main())
---
+++ examples/table_manager_hook.py
@@ -0,0 +1,75 @@
+import asyncio
+from datetime import date
+
+from asyncpg_utils.databases import Database
+from asyncpg_utils.managers import AbstractHook, TableManager
+
+loop = asyncio.get_event_loop()
+database = Database('postgresql://postgres:postgres@localhost/asyncpg-utils')
+user_data = {
+    'name': 'Allisson',
+    'dob': date(1983, 2, 9)
+}
+
+
+class TestHook(AbstractHook):
+    async def pre_create(self, data):
+        print('pre_create, data={!r}'.format(data))
+
+    async def post_create(self, row):
+        print('post_create, row={!r}'.format(row))
+
+    async def pre_list(self, fields, filters):
+        print('pre_list, fields={!r}, filters={!r}'.format(fields, filters))
+
+    async def post_list(self, rows):
+        print('post_list, rows={!r}'.format(rows))
+
+    async def pre_detail(self, pk, fields):
+        print('pre_detail, pk={!r}, fields={!r}'.format(pk, fields))
+
+    async def post_detail(self, row):
+        print('post_detail, row={!r}'.format(row))
+
+    async def pre_update(self, pk, data):
+        print('pre_update, pk={!r}, data={!r}'.format(pk, data))
+
+    async def post_update(self, row):
+        print('post_update, row={!r}'.format(row))
+
+    async def pre_delete(self, pk):
+        print('pre_delete, pk={!r}'.format(pk))
+
+    async def post_delete(self, pk):
+        print('post_delete, pk={!r}'.format(pk))
+
+
+table_manager = TableManager(database, 'users', pk_field='id', hooks=(TestHook,))
+
+
+async def create_table():
+    conn = await database.get_connection()
+    await conn.execute(
+        """
+        CREATE TABLE IF NOT EXISTS users(
+            id serial PRIMARY KEY,
+            name text,
+            dob date
+        )
+        """
+    )
+    await conn.close()
+    return True
+
+
+async def main():
+    print('create_table users, {!r}'.format(await create_table()))
+    await table_manager.create(user_data)
+    await table_manager.list()
+    await table_manager.detail(1)
+    user_data['name'] = 'John Doe'
+    await table_manager.update(1, user_data)
+    await table_manager.delete(1)
+
+
+loop.run_until_complete(main())
